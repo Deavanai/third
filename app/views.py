9cceeb36fd3c6d2ca72c503ab889d814585533de
Deavanai <yokeshbabu18091999@gmail.com>
2022-06-23 13:59:24 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hi(request):
+    return render(request,'hi.html')
+def hello(request):
+    return render(request,'hello.html')
